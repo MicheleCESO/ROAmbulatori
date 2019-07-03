96847f1814e1b566ad5435bcce733c38909ae57e
MicheleCESO <ceso91.mc@gmail.com>
2019-07-03 17:10:55 +0200

    Avanzamento algoritmo greedy

--- progetto.py
+++ progetto.py
@@ -1,7 +1,7 @@
 from IstGen import genIstanza
+import itertools
 
 pi = [1,2,3,4,5]
-
 
 def pprint(ist):
 	print(ist,"\n")
@@ -28,21 +28,49 @@ def risolvi(ist):
 	ist.sort(key=len)
 	ist.reverse()
 
+	startP = [0] * len(ist) # Start pazienti
+	startJ = [{}] * len(ist) # I dizionari devono essere separati
 	stanzeLibere = 3
 	statoStanze = [0,0,0]
 	index = 1
 
-	listaPazienti = [ist[0]]
+	listaPazienti = []
 
-	for :
+	#statoStanze[minimo] += sum(ist[0])
 
+
+		while :
+			conflitto,lista = confronto(ist,startP,listaPazienti,index) # Confrontiamo paziente scelto attuale con quelli precedentemente scelti
+			index += 1
 		minimo = minStato(statoStanze)
-		if : # Ho già scelto altri pazienti e devono essere confrontati
-			conflitto,lista = confronto(listaPazienti,ist(index)) # Confrontimo paziente scelto attuale con quelli precedentemente scelti
-		index += 1
-		stanzeLibere -= 1
 
-def confronto(lista,paziente):
+
+
+# Gestisce il confronto tra i pazienti già inseriti e uno nuovo
+def confronto(istanza, startPaziente, pazientiInAmbulatorio, nuovoPaziente):
+
+	# Permutazioni possibili del nuovo paziente	
+	lista3 = permuta(istanza[nuovoPaziente])
+	if len(pazientiInAmbulatorio) == 0:
+		return 0,istanza[nuovoPaziente]
+	elif len(pazientiInAmbulatorio) == 1:
+		lista2 = None
+ 
+ 	costo = 9999
+ 	sol = None
+	for p3 in lista3:  
+		livelloConflitto = verifica(istanza[pazientiInAmbulatorio[0]],lista2,p3)
+		if livelloConflitto < costo:
+			costo = livelloConflitto
+			sol = p3
+
+	return costo,sol
+
+def verifica(L1,L2,L3):
+	for el1 in L1:
+		if el1 in L3: # Se ci sono due esami dello stesso tipo
+
+	if L2:		
 
 
 
